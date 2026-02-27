dcd6611d5916752196e5eae29ed19d4fa5289681
umatrix81 <umatrix81@gmail.com>
2026-02-27 12:53:51 +0300

    now accepts domains too

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ use std::env::args;
 use std::env::home_dir;
 use std::fs::File;
 use std::io::Read;
-use std::net::Ipv4Addr;
+// use std::net::Ipv4Addr;
 use std::path::PathBuf;
 
-fn check_ip_format(ip: &str) -> bool {
-    match ip.parse::<Ipv4Addr>() {
-        Ok(_addr) => true,
-        Err(_) => false,
-    }
-}
+// fn check_ip_format(ip: &str) -> bool {
+//     match ip.parse::<Ipv4Addr>() {
+//         Ok(_addr) => true,
+//         Err(_) => false,
+//     }
+// }
 
 fn main() {
     let args: Vec<String> = args().collect();
@@ -18,11 +18,11 @@ fn main() {
         eprintln!("Usage: {} <ip address to remove>", args[0]);
         std::process::exit(0);
     }
-    if !check_ip_format(&args[1]) {
-        eprintln!("Invalid IP address format. Please provide a valid IPv4 address.");
-        std::process::exit(0);
-    }
-    let ip_to_remove = &args[1];
+    // if !check_ip_format(&args[1]) {
+    //     eprintln!("Invalid IP address format. Please provide a valid IPv4 address.");
+    //     std::process::exit(0);
+    // }
+    let host_to_remove = &args[1];
 
     let homedir = home_dir().expect("Failed to get home directory");
     let known_hosts_path: PathBuf = homedir.join(".ssh\\known_hosts");
@@ -34,16 +34,16 @@ fn main() {
 
     let occurrences = contents
         .lines()
-        .filter(|line| line.contains(ip_to_remove))
+        .filter(|line| line.contains(host_to_remove))
         .count();
     if occurrences > 0 {
         println!(
             "Found {} occurrence(s) of {:?} in known_hosts.",
-            occurrences, ip_to_remove
+            occurrences, host_to_remove
         );
         let filtered: String = contents
             .lines()
-            .filter(|line| !line.contains(ip_to_remove))
+            .filter(|line| !line.contains(host_to_remove))
             .map(|line| format!("{}\n", line))
             .collect();
 
@@ -51,9 +51,12 @@ fn main() {
             .expect("Failed to write updated known_hosts file");
         println!(
             "Removed all occurrences of {:?} from known_hosts.",
-            ip_to_remove
+            host_to_remove
         );
     } else {
-        println!("No occurrences of {:?} found in known_hosts.", ip_to_remove);
+        println!(
+            "No occurrences of {:?} found in known_hosts.",
+            host_to_remove
+        );
     }
 }
